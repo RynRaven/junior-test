809179b0d3e6b9bb3052de8774c4b53bfb6e7287
Ryn <msraven.game@gmail.com>
2022-02-02 14:48:54 +0200

    Workplace starting setup
    Added main component files

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ProductCard.scss';
+
+export class ProductCard extends Component {
+    render() {
+    return (
+        <div className="ProductCard">
+            <h1>Product Card</h1>
+        </div>
+    )
+    }
+}
+
+export default ProductCard;
